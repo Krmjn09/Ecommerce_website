11292cebb893d85eac11aea15db0e072d95a1d16
Krmjn09 <kmahajan1_be22@thapar.edu>
2024-06-30 12:23:38 +0530

    commited

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import './AddProduct.css';
+import React from "react"
+import "./AddProduct.css"
 const AddProduct = () => {
-    return (
-        <div className="add-product">
-            <div className=''
+  return (
+    <div className="add-product">
+      <div className="addproduct-itemfield">
+        <p>Product title</p>
+        <input type="text" name="name" placeholder="Enter product title" />
+      </div>
+      <div className="addproduct-price">
+        <div className="addproduct-itemfield">
+          <p>Price</p>
+          <input
+            type="text"
+            name="old_price"
+            placeholder="Enter product price"
+          />
         </div>
-    )
+        <div className="addproduct-itemfield">
+          <p>Discounted Price</p>
+          <input
+            type="text"
+            name="new_price"
+            placeholder="Enter discounted price"
+          />
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Product category</p>
+          <select name="category" className="add-product-selector">
+            <option value="women">Women</option>
+            <option value="men">Men</option>
+            <option value="kid">Kid</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
 }
-export default AddProduct;
+export default AddProduct
